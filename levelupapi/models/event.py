8410b24b7df88792f2158973c2e0dc45e1007adb
Matthew Hedges <matthedges35@gmail.com>
2023-02-15 14:33:05 -0600

    models fixed

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Event(models.Model):
 
-    organizer = models.ForeignKey('Gamer', on_delete=models.CASCADE, related_name='events_createx_by_user')
+    organizer = models.ForeignKey('Gamer', on_delete=models.CASCADE, related_name='events_created_by_user')
     name = models.CharField(max_length=50)
     date = models.DateField(null=True, blank=True, auto_now=False, auto_now_add=False)
     location = models.CharField(max_length=200)
